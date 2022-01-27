da9ed5170440372201141c9607dfb199ef9e289d
mateusc91 <mateusc.eng@gmail.com>
2022-01-27 09:02:13 -0500

    adicionando sql scripts

--- src/main/java/com/example/testeyoux/TesteYouxApplication.java
+++ src/main/java/com/example/testeyoux/TesteYouxApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = PacienteRepositorio.class)
+
 public class TesteYouxApplication {
 
     public static void main(String[] args) {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios (cpf,funcao,nome,senha)
+VALUES ('123', 'enfermeiro', 'usuario_enfermeiro','$2a$10$1dCKuQoQqbBNCK.Rb8XQSemwqdHdVAcCTb1kUQLg2key/4VX./TvS');
+
+INSERT INTO usuarios (cpf,funcao,nome,senha)
+VALUES ('1234', 'medico', 'usuario_medico','$2a$10$1dCKuQoQqbBNCK.Rb8XQSemwqdHdVAcCTb1kUQLg2key/4VX./TvS');
